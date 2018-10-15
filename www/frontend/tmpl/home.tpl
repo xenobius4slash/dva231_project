f0455e22e5eb4987f92deed17c6694088dca5ba8
Iván Muñiz <ivanmr_96@live.com.mx>
2018-10-15 22:21:08 +0200

    interface preview

--- www/frontend/tmpl/home.tpl
+++ www/frontend/tmpl/home.tpl
@@ -1,1 +1,29 @@
-HOME - PAGE
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="container">
+		<div class="row rstyle">
+			<div class="col-md-4 col-sm-6">
+				<div class="cstyle">
+					<p>Wheather: </p>
+				</div>
+			</div>
+			<!-- <div class="col-sm-1"></div> -->
+			<div class="col-md-4 col-sm-6">
+				<div class="cstyle">
+					Wheather
+				</div>
+			</div>
+			<!-- <div class="col-sm-1"></div> -->
+			<div class="col-md-4 col-sm-6">
+				<div class="cstyle">
+					Wheather
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
--- www/index.php
+++ www/index.php
@@ -31,33 +31,85 @@ if( isset($_GET['page']) ) {
 		<title>DVA231 - Weather Compare</title>
 
 		<!-- Bootstrap -->
-<!--
-		<link href="<?=CSS_PATH?>bootstrap.min.css" rel="stylesheet" type="text/css" />
-		<link href="<?=CSS_PATH?>rpow.css" rel="stylesheet" type="text/css" />
--->
+		<link href="<?=CSS_PATH?>bootstrap.css" rel="stylesheet" type="text/css" />
+		<link href="<?=CSS_PATH?>index.css" rel="stylesheet" type="text/css" />
+		<!-- <link href="<?=CSS_PATH?>rpow.css" rel="stylesheet" type="text/css" /> -->
+
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<!--
-		<script src="<?=JS_PATH?>jquery-2.1.4.min.js"></script>
--->
+
+		<script src="<?=JS_PATH?>jquery-3.3.1.js"></script>
+
 	</head>
 	<body>
-		<nav>
-			NAVIGATION
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+			<!-- TO GIVE THE NAVBAR SOME MARGIN FROM THE BORDERS -->
+			<div class="container">
+
+				<!-- NAVBAR LOGO -->
+
+				<div class="navbar-header">
+					<a href="#" class="navbar-brand">Weather</a>
+				</div>
+
+				<!-- LEFT PART OF THE NAVBAR -->
+
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contact</a></li>
+						<form class="navbar-form navbar-left" role="search">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Search">
+							</div>
+						<button type="submit" class="btn btn-default">Submit</button>
+					</form>
+
+					</ul>
+
+					<!-- RIGHT PART OF THE NAVBAR -->
+
+					<ul class="nav navbar-nav navbar-right">
+						<!-- <li><a href="#">Sign Up</a></li> -->
+						<li><a href="" data-toggle="modal" data-target="#login-modal">Login</a></li>
+					</ul>
+
+				</div>
+			</div>
 		</nav>
+
+ 		<div id="login-modal" class="modal fade" role="dialog" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+ 			<div class="modal-dialog">
+ 				<div class="loginmodal-container">
+ 					<h1 class="">Login to your account</h1>
+ 					<form>
+					 	<input type="text" name="user" placeholder="Username">
+					 	<input type="password" name="pass" placeholder="Password">
+					 	<input type="submit" name="login" value="Login" class="btn btn-success loginmodal-submit">
+					</form>
+					<div class="loginmodal-a">
+						<a href="#">Register</a> - <a href="#">Forgot Password</a>
+					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+
+
 
 		<section id="main_container">
 			<?php 
-			if($phpfile) { require_once SCRIPT_PATH.$page.'.php'; }
-			require_once TMPL_PATH.$page.'.tpl';
-			?>
+				if($phpfile) { 
+					require_once SCRIPT_PATH.$page.'.php';
+				}
+				require_once TMPL_PATH.$page.'.tpl';
+			?> 
 		</section>
 
 		<footer>
 			FOOTER
 		</footer>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
-<!--
-		<script src="<?=JS_PATH?>bootstrap.min.js"></script>
--->
+
+		<script src="<?=JS_PATH?>bootstrap.js"></script>
+
 	</body>
 </html>
