8b670ebc5d20e575f5b5297bfc07789f1e0edd73
sui18001 <sui18001@student.mdh.se>
2018-10-30 20:39:49 +0100

    use php file for user and enable admin page

--- www/index.php
+++ www/index.php
@@ -39,8 +39,8 @@ if( isset($_GET['page']) ) {
 		case 'home' : $page = 'home';  break;
 		case 'town' : $page = 'town'; $phpfile = true; break;
 		case 'test' : $page = 'test'; $phpfile = true; break;
-		case 'user' : $page = 'user'; $phpfile = false; break;
-//		case 'admin' : if($admin) { $page = 'admin'; $phpfile = true; } else { $page = 'home'; } break;
+		case 'user' : $page = 'user'; $phpfile = true; break;
+		case 'admin' : if($admin) { $page = 'admin'; $phpfile = true; } else { $page = 'home'; } break;
 //		case 'imprint': $page = 'imprint'; break;
 		default : $page = 'home';
 	}
